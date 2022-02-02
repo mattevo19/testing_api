f75f2590af44580a8e72b75725e1c50e216f8da3
matt <mattevo10@gmail.com>
2022-02-02 12:39:35 +0100

    added seeds

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ gem "bootsnap", require: false
 gem 'devise'
 gem "pundit"
 # gem 'simple_token_authentication'
+gem 'faker'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+User.destroy_all
+puts 'Users deleted'
+Gym.destroy_all
+puts 'Users deleted'
+Comment.destroy_all
+puts 'Comments deleted'
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+5.times do |i|
+  User.create!(email:Faker::Internet.email, password:'testtest1')
+  puts "User #{i+1} created"
+end
+
+10.times do |i|
+  Gym.create!(name:Faker::Mountain.name,address:Faker::Address.street_address,user_id:(rand(1..5)))
+  puts "Gym #{i+1} created"
+end
+
+10.times do |i|
+  Comment.create!(content: Faker::Movie.quote, gym_id: "#{i+1}".to_i , user_id: rand(1..5))
+  Comment.create!(content: Faker::Movie.quote, gym_id: "#{i+1}".to_i , user_id: rand(1..5))
+  Comment.create!(content: Faker::Movie.quote, gym_id: "#{i+1}".to_i , user_id: rand(1..5))
+end
+puts "Comments created"
