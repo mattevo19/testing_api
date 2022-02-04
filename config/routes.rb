af4ee1205dc93e1f296ddc35574461a140f5284d
matt <mattevo10@gmail.com>
2022-02-04 15:48:36 +0100

    adjusting

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :gyms, only: [ :index,:show,:update,:create, :destroy ]
     end
   end
-  # Defines the root path route ("/")
+
+  root to: "gyms#index"
   resources :gyms, only: [:show] do
     resources :comments, only: [:create]
   end
-  root to: "gyms#index"
 end
